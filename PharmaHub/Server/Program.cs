79e5a55e2d8e107d22e0a14a72ce0568f516ce78
SCOTT LEE CHUN HONG 2203891C <2203891C@student.tp.edu.sg>
2023-12-10 21:44:47 +0800

    Added Identity Server

--- PharmaHub/Server/Program.cs
+++ PharmaHub/Server/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using PharmaHub.Server.Data;
 using PharmaHub.Server.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
